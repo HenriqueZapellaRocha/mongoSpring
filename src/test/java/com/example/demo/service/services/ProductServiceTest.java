b958b9b96a93985f8972a543c02dc921c11d2427
henriqueZapellaRocha <henri_zap@hotmail.com>
2024-10-03 16:01:35 -0300

    added get by id productService basic teste

--- src/test/java/com/example/demo/service/services/ProductServiceTest.java
+++ src/test/java/com/example/demo/service/services/ProductServiceTest.java
@@ -3,15 +3,15 @@ package com.example.demo.service.services;
 import com.example.demo.repository.ProductRepository;
 import com.example.demo.repository.entity.ProductEntity;
 import com.example.demo.v1.controller.ProductRequestDTO;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -46,22 +46,25 @@ class ProductServiceTest {
         assertNotNull(product.getProductID());
         assertEquals(200 * 0.1824, product.getPrice());
         assertEquals(productRequestDTO.getName(), product.getName());
-
+        assertEquals(product, productRepository.findById(product.getProductID()).orElse(null));
     }
-
     @Test
-    void getById() {
-    }
+    @DisplayName("get by ID test")
+    void getByIdTest() {
+       ProductEntity product = ProductEntity.builder()
+                                                    .productID(UUID.randomUUID().toString())
+                                                    .name("Clang").price(200.0)
+                                                    .build();
 
-    @Test
-    void update() {
-    }
+       when(exchangeService.makeExchange("USD", "BRL")).thenReturn(5.44);
 
-    @Test
-    void getAll() {
-    }
+       productRepository.save(product);
+       ProductEntity productRecived = productService.getById(product.getProductID(),"USD", "BRL");
 
-    @Test
-    void deleteMany() {
+       assertNotNull(productRecived);
+       assertNotNull(productRecived.getProductID());
+       assertEquals(product.getProductID(), productRecived.getProductID());
+       assertEquals(product.getName(), productRecived.getName());
+       assertEquals(product.getPrice() * 5.44, productRecived.getPrice());
     }
 }
